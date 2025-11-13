ccdf46ae58e9d2a338de7d31b2645f05895bc2ec
Enzoprogramerr <enzomedina630@gmail.com>
2025-11-13 10:02:31 -0300

    Configuración inicial par github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
 import CasaRoute from "./pages/CasaRoute.tsx";
 import DuplexRoute from "./pages/DuplexRoute.tsx";
@@ -6,14 +6,14 @@ import TerminosRoute from "./pages/TerminosRoute.tsx";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/casa" element={<CasaRoute />} />
         <Route path="/duplex" element={<DuplexRoute />} />
         <Route path="/terminos" element={<TerminosRoute />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
